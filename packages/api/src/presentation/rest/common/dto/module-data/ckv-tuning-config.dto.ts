3f106f39741084d23c4d4065691caf0ec176a82c
Amit Gupta <amigup@qti.qualcomm.com>
2026-03-23 14:11:24 +0530

    feat(api): add driver module DTOs to common
    Extracted common parts of SPF module and driver module into
    base-module.dto that both driver and SPF modules extend. Added
    these DTOs to common folder for reusability across features like
    patch summary, diff-merge, and other module operations.
    
    Signed-off-by: Amit Gupta <amigup@qti.qualcomm.com>

---
+++ packages/api/src/presentation/rest/common/dto/module-data/ckv-tuning-config.dto.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {ApiProperty} from '@nestjs/swagger';
+import {ParameterSummaryDto} from '../parameter.dto.js';
+import {KeyValuePairsDto} from '../key-value.dto.js';
+
+/**
+ * CKV Tuning Configuration DTO containing associated CKVs and supported parameters.
+ */
+export class CkvTuningConfigDto {
+  @ApiProperty({
+    description: 'Associated key-value pairs',
+    type: [KeyValuePairsDto],
+  })
+  associatedKvs!: KeyValuePairsDto[];
+
+  @ApiProperty({
+    description: 'Supported parameter information',
+    type: ParameterSummaryDto,
+  })
+  supportedParameters!: ParameterSummaryDto;
+}
