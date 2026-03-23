c15fbbc32aa4ebdcde8eb0ddcf6e29563695181b
Amit Gupta <amigup@qti.qualcomm.com>
2026-03-23 14:11:24 +0530

    feat(api): add driver module DTOs to common
    Extracted common parts of SPF module and driver module into
    base-module.dto that both driver and SPF modules extend. Added
    these DTOs to common folder for reusability across features like
    patch summary, diff-merge, and other module operations.
    
    Signed-off-by: Amit Gupta <amigup@qti.qualcomm.com>

---
+++ packages/api/src/presentation/rest/common/dto/module-data/driver-module.dto.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {BaseModuleSummaryDto, BaseModuleDto} from './base-module.dto.js';
+export class DriverModuleSummaryDto extends BaseModuleSummaryDto {}
+
+export class DriverModuleDto extends BaseModuleDto {}
