813803bf58791efd873d886e1509a6477c8056cb
Tapan Mohakul <tmohakul@qti.qualcomm.com>
2026-04-13 16:40:31 +0530

    feat(persistence):flatten bulk insert error
    Removed hierarchical error structure from base class as errors now
    contain all required context information eliminating need for nested
    error traversal.
    
    Updated BaseHierarchicalEntityResult to store errors directly without
    child hierarchy. Modified bulk insert result classes to compute
    summaries from flat error collections.
    
    Signed-off-by: Tapan Mohakul <tmohakul@qti.qualcomm.com>

---
+++ packages/core/src/application/ports/persistence/repositories/bulk-import/bulk-import-interface/base-insert-error.interface.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export interface BaseInsertError {
+  /** System ID of the failing entity */
+  systemId: number;
+
+  /** Human-readable error message */
+  message: string;
+}
